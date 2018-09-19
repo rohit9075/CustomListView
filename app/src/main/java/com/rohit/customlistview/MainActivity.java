04126b8e0e484fa1a920ca2ea00ea15e7ca3cb11
Rohit Kumar <rohit9075@gmail.com>
2018-09-19 10:20:56 +0530

    code updates

--- app/src/main/java/com/rohit/customlistview/MainActivity.java
+++ app/src/main/java/com/rohit/customlistview/MainActivity.java
@@ -2,6 +2,13 @@ package com.rohit.customlistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.rohit.customlistview.StudentAdapter.StudentListAdapter;
+import com.rohit.customlistview.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Student> studentList = new ArrayList<>();
+
+        for (int i = 0; i < 50; i++) {
+
+            Student student = new Student();
+            student.setName("ABC");
+            student.setId("0012cs12035");
+            student.setCity("Bangalore");
+
+            studentList.add(student);
+
+        }
+
+        StudentListAdapter studentListAdapter = new StudentListAdapter(MainActivity.this,R.layout.item_list, studentList);
+
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(studentListAdapter);
+
     }
 }
--- app/src/main/java/com/rohit/customlistview/StudentAdapter/StudentListAdapter.java
+++ app/src/main/java/com/rohit/customlistview/StudentAdapter/StudentListAdapter.java
@@ -1,9 +1,6 @@
 package com.rohit.customlistview.StudentAdapter;
 
-import android.app.Activity;
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
