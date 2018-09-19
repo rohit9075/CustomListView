4095522c5a377953ce3cbf81b05c4791fd5295b8
Rohit Kumar <rohit9075@gmail.com>
2018-09-19 10:14:45 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/customlistview/StudentAdapter/StudentListAdapter.java
@@ -0,0 +1,53 @@
+package com.rohit.customlistview.StudentAdapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.rohit.customlistview.R;
+import com.rohit.customlistview.model.Student;
+
+import java.util.List;
+
+public class StudentListAdapter extends ArrayAdapter<Student> {
+
+    List<Student> studentList ;
+    Context mContext;
+
+    public StudentListAdapter(Context context, int resource, List<Student> studentList) {
+        super(context, resource , studentList);
+        this.studentList = studentList;
+        mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null){
+//            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+//            convertView = inflater.inflate(R.layout.item_list,null , true);
+
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_list,null,false);
+        }
+
+        Student student = getItem(position);
+
+        TextView mTextViewName = convertView.findViewById(R.id.textView_name);
+        mTextViewName.setText(student.getName());
+
+        TextView mTextViewId = convertView.findViewById(R.id.textView_id);
+        mTextViewId.setText(student.getId());
+
+        TextView mTextViewCity = convertView.findViewById(R.id.textView_city);
+        mTextViewCity.setText(student.getCity());
+
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/rohit/customlistview/model/Student.java
@@ -0,0 +1,29 @@
+package com.rohit.customlistview.model;
+
+public class Student {
+    private String name, id , city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
